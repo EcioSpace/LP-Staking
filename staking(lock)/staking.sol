50bc3b4cb5d0ac5a4d9cba4ba8187d43f1c39147
AgfeLiger <93938480+AgfeLiger@users.noreply.github.com>
2022-05-10 10:21:00 +0900

    Reward system update
    reward system update

--- staking(lock)/staking.sol
+++ staking(lock)/staking.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.4;
-import './Math.sol';
+// import './Math.sol';
+import './SafeMath.sol';
 import './TransferHelper.sol';
 import './IERC20.sol';
 
@@ -19,6 +20,7 @@ contract EcioStaking {
 
     // pool info
     address public lpToken;
+    // IERC20 lpToken;
     uint256 public totalAmount;
 
     address public rewardToken;
@@ -26,6 +28,8 @@ contract EcioStaking {
     // Reward tokens created per Sec.
     uint256 public rewardRate;
     
+
+    uint256 public totalAmountLockDay;
     // Info of each user that stakes LP tokens.
     mapping (address => UserInfo) public userInfo;
     mapping(address => mapping (address => uint256)) allowed;
@@ -35,22 +39,17 @@ contract EcioStaking {
     event Deposit(address indexed user, uint256 amount);
     event Withdraw(address indexed user, uint256 amount);
     event Reward(address indexed user, uint256 amount);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 
     constructor(
         address _lpToken,
-        address _rewardToken,
-        uint256 _rewardRate
+        // IERC20 _lpToken,
+        address _rewardToken
     ) public {
         adminAddress = msg.sender;
         lpToken = _lpToken;
         rewardToken = _rewardToken;
-        rewardRate = _rewardRate;
         totalAmount = 0;
-    }
-
-    function lockedPeriod(uint256 lockDay) public returns (uint256) {
-        return lockDay * 24 * 3600;
+        totalAmountLockDay = 0;
     }
 
     function setAdmin(address _adminAddress) public {
@@ -63,12 +62,14 @@ contract EcioStaking {
         return user.amount;
     }
 
-    function multiplier() public view returns (uint256) {
+    function multiplier(uint256 lockDays) public view returns (uint256) {
         UserInfo storage user = userInfo[msg.sender];
-        if(user.lockedDay == 30) return 1;
-        if(user.lockedDay == 60) return 2;
-        if(user.lockedDay == 90) return 3;
-        if(user.lockedDay == 120) return 4;
+        if(lockDays == 30) return 10;
+        if(lockDays == 60) return 15;
+        if(lockDays == 90) return 20;
+        if(lockDays == 120) return 30;
+        if(lockDays == 240) return 40;
+        if(lockDays == 360) return 50;
     }
 
     function stakingPeriod() public view returns (uint256) {
@@ -88,27 +89,24 @@ contract EcioStaking {
 
     function stakingStatus() public view returns (bool) {
         UserInfo storage user = userInfo[msg.sender];
-        if(user.lastDepositTimeStamp + user.lockedDay > block.timestamp) return false;
+        if(user.lastDepositTimeStamp + user.lockedDay * 1 days > block.timestamp) return false;
         else return true;
     }
 
-    function approve(uint256 numTokens) public returns (bool) {
-        allowed[msg.sender][address(this)] = numTokens;
-        emit Approval(msg.sender, address(this), numTokens);
-        return true;
-    }
-
     function updatePool() internal {
-        for (uint i = 0; i < userList.length; i++) {
+        for(uint i = 0 ; i < userList.length ; i ++){
             UserInfo storage user = userInfo[userList[i]];
             uint256 lastTimeStamp = block.timestamp;
-            uint256 accDebt = lastTimeStamp.sub(user.lastCalculatedTimeStamp).mul(rewardRate).mul(1e18) / totalAmount;
-            user.rewardDebt = user.rewardDebt.add(accDebt);
-            user.lastCalculatedTimeStamp = lastTimeStamp;
+            if(user.lastCalculatedTimeStamp + 1 days <= lastTimeStamp){
+                // user.lastDepositTimeStamp = lastTimeStamp;
+                uint256 accDebt = (user.amount * multiplier(user.lockedDay) * (1e6) * (1e18)) / totalAmountLockDay;
+                user.rewardDebt = user.rewardDebt.add(accDebt);
+                user.lastCalculatedTimeStamp = lastTimeStamp;
+            }
         }
     }
 
-    function deposit(uint256 amount, uint256 lockedDay) public {
+    function deposit(uint256 amount, uint256 lockDay) public {
         require(amount > 0, "invaild amount");
         TransferHelper.safeTransferFrom(lpToken, msg.sender, address(this), amount);
         UserInfo storage user = userInfo[msg.sender];
@@ -125,13 +123,16 @@ contract EcioStaking {
             user.rewarded = 0;
             user.rewardDebt = 0;
             user.lastDepositTimeStamp = block.timestamp;
-            user.lockedDay = lockedDay;            
+            user.lastCalculatedTimeStamp = block.timestamp;
+            user.lockedDay = lockDay;            
         } else {
             user.amount = user.amount + amount;
             user.lastDepositTimeStamp = block.timestamp;
-            user.lockedDay = lockedDay;
+            user.lastCalculatedTimeStamp = block.timestamp;
+            user.lockedDay = lockDay;
         }
         totalAmount = totalAmount + amount;
+        totalAmountLockDay = totalAmountLockDay.add(amount * multiplier(lockDay));
         emit Deposit(msg.sender, amount);
     }
 
@@ -139,7 +140,7 @@ contract EcioStaking {
         UserInfo storage user = userInfo[msg.sender];
         require(user.lastDepositTimeStamp > 0, "invalid user");
         require(user.amount > 0, "not staked");
-        require(user.lastDepositTimeStamp + lockedPeriod(user.lockedDay) < block.timestamp, "you are in lockedTime.");
+        require(user.lastDepositTimeStamp + user.lockedDay * 1 days < block.timestamp, "you are in lockedTime.");
         updatePool();
         TransferHelper.safeTransfer(lpToken, msg.sender, user.amount);
         totalAmount = totalAmount - user.amount;
@@ -149,11 +150,18 @@ contract EcioStaking {
         emit Withdraw(msg.sender, user.amount);
     }
 
-    function reward() public {
+    function rewardUpdate() public {
+        UserInfo storage user = userInfo[msg.sender];
+        updatePool();
+        user.rewarded = user.rewarded + user.rewardDebt;
+        user.rewardDebt = 0;
+    }
+
+    function claim() public {
         UserInfo storage user = userInfo[msg.sender];
         updatePool();
         uint amount = user.rewardDebt;
-        require(amount > 0, "not enough reward amount");
+        // require(amount > 0, "not enough reward amount");
         user.rewarded = user.rewarded + amount;
         user.rewardDebt = 0;
         TransferHelper.safeTransfer(rewardToken, msg.sender, amount);
